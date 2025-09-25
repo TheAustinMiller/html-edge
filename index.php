5ae9c8cd9287a16e806d5374050402468b83263b
TheAustinMiller <themilleraustin@gmail.com>
2025-09-24 21:31:30 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    include 'header.html';
+?>
+<!DOCTYPE html>
+<head>
+    <title>Edge Fitness and Sports Performance</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<html>
+    <body>
+        <div class="content">
+            <h2>Welcome to Edge Fitness and Sports Performance</h2>
+            <p>Your journey to peak performance starts here. Explore our services and join our community.</p>
+        </div>
+    </body>
+</html>
+<?php
+    include 'footer.html';
+?>
